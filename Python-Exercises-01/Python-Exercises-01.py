2c077a575e72c065f5aa5ac43866d94772f0cb75
AliZokaeai <47479098+AliZokaeai@users.noreply.github.com>
2020-10-10 21:35:56 +0330

    My first Python Exercise 01

--- Python-Exercises-01/Python-Exercises-01.py
+++ Python-Exercises-01/Python-Exercises-01.py
@@ -1,1 +1,10 @@
-print("Hello World")
+my_var = 10
+print('my-var = {0}'. format(my_var))
+print('memory address of my-var (decimal): {0}'.format(id(my_var)))
+print('memory address of my-var (hex):{0}'. format(hex(id(my_var))))
+
+greeting = 'hello'
+print('greeting = {0}'. format(greeting))
+print('memory address of my-var (decimal): {0}'.format(id(greeting)))
+print('memory address of my-var (hex):{0}'.format(hex(id(greeting))))
+
